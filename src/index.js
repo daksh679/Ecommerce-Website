5a457f396e345d14b2e9aa107270453c0babc0a2
Daksh Ahuja <adaksh679@gmail.com>
2024-12-19 23:30:45 +0530

    Store Setup

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 const App = lazy(() => import("./App"));
+import { Provider } from "react-redux";
+import store from "./store/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Suspense>
-      <App />
-    </Suspense>
+    <Provider store={store}>
+      <Suspense>
+        <App />
+      </Suspense>
+    </Provider>
   </BrowserRouter>
 );
---
+++ src/store/rootReducer.js
@@ -0,0 +1,7 @@
+import { authReducer } from "./Reducers/AuthReducer";
+
+const rootReducer = {
+  auth: authReducer,
+};
+
+export default rootReducer;
