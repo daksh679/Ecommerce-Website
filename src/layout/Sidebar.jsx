4010ffcac28bed960c461c685f621550eeb9c4ed
Daksh Ahuja <adaksh679@gmail.com>
2024-12-25 21:06:09 +0530

    Home Page Build

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,7 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { getNavs } from "./../navigation/index";
 
 const Sidebar = () => {
+  const { pathName } = useLocation();
+  const [allNav, setAllNav] = useState([]);
+
+  useEffect(() => {
+    const navs = getNavs("admin");
+    setAllNav(navs);
+  }, []);
+
+  console.log(allNav);
   return (
     <div>
       <div></div>
@@ -14,6 +24,25 @@ const Sidebar = () => {
               alt=""
             />
           </Link>
+        </div>
+        <div className="m-4">
+          <ul>
+            {allNav.map((n, i) => (
+              <li key={i}>
+                <Link
+                  to={n.path}
+                  className={`${
+                    pathName === n.path
+                      ? " bg-blue-500 shadow-indigo-500/50 text-white duration-500"
+                      : " text-[#030811] font-bold duration-200"
+                  } px-[12px] py-[9px]`}
+                >
+                  <span>{n.icon}</span>
+                  <span>{n.title}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
---
+++ src/navigation/allNav.js
@@ -0,0 +1,11 @@
+import { AiOutlineDashboard } from "react-icons/ai";
+
+export const allNav = [
+  {
+    id: 1,
+    title: "Dashboard",
+    role: "admin",
+    icon: <AiOutlineDashboard />,
+    path: "/admin/dashboard",
+  },
+];
