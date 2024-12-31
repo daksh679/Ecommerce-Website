17b736ebf347558357e0cd567e8e177acb1d1213
Daksh Ahuja <adaksh679@gmail.com>
2024-12-31 19:31:08 +0530

    Dashboard/Sellers

--- src/router/routes/adminRoutes.js
+++ src/router/routes/adminRoutes.js
@@ -2,6 +2,13 @@ import { lazy } from "react";
 const AdminDashboard = lazy(() => import("./../../views/admin/AdminDashboard"));
 const Orders = lazy(() => import("./../../views/admin/Orders"));
 const Category = lazy(() => import("./../../views/admin/Category"));
+const Sellers = lazy(() => import("./../../views/admin/Sellers"));
+const PaymentRequest = lazy(() => import("./../../views/admin/PaymentRequest"));
+const DeactiveSellers = lazy(() =>
+  import("./../../views/admin/DeactiveSellers")
+);
+const SellersRequest = lazy(() => import("./../../views/admin/SellersRequest"));
+const LiveChat = lazy(() => import("./../../views/admin/LiveChat"));
 
 export const adminRoutes = [
   {
@@ -17,6 +24,31 @@ export const adminRoutes = [
   {
     path: "admin/dashboard/category",
     element: <Category />,
+    role: "admin",
+  },
+  {
+    path: "admin/dashboard/sellers",
+    element: <Sellers />,
+    role: "admin",
+  },
+  {
+    path: "admin/dashboard/payment-request",
+    element: <PaymentRequest />,
+    role: "admin",
+  },
+  {
+    path: "admin/dashboard/deactive-sellers",
+    element: <DeactiveSellers />,
+    role: "admin",
+  },
+  {
+    path: "admin/dashboard/sellers-request",
+    element: <SellersRequest />,
+    role: "admin",
+  },
+  {
+    path: "admin/dashboard/chat-sellers",
+    element: <LiveChat />,
     role: "admin",
   },
 ];
