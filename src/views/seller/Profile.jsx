25fb861fd8650656168764f635bb4fa495976fec
Daksh Ahuja <adaksh679@gmail.com>
2025-02-01 19:53:22 +0530

    Seller Dashboard/Profile

--- src/views/seller/Profile.jsx
+++ src/views/seller/Profile.jsx
@@ -1,130 +1,119 @@
 import React from "react";
-import { AiFillProduct } from "react-icons/ai";
-import { FaCartArrowDown, FaShoppingCart } from "react-icons/fa";
-import { MdCurrencyRupee } from "react-icons/md";
-import Chart from "react-apexcharts";
-import { Link } from "react-router-dom";
 
 const Profile = () => {
-  const state = {
-    series: [
-      {
-        name: "Orders",
-        data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
-      },
-      {
-        name: "Revenue",
-        data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
-      },
-      {
-        name: "Sales",
-        data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
-      },
-    ],
-    options: {
-      color: ["#181ee8", "#181ee8"],
-      plotOptions: {
-        radius: 30,
-      },
-      chart: {
-        background: "transparent",
-        foreColor: "#d0d2d6",
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        show: true,
-        curve: ["smooth", "straight", "stepline"],
-        lineCap: "butt",
-        colors: "#0f0f0f",
-        width: 0.5,
-        dashArray: 0,
-      },
-      xaxis: {
-        categories: [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep",
-          "Oct",
-          "Nov",
-          "Dec",
-        ],
-      },
-      legend: {
-        position: "top",
-      },
-      Responseive: [
-        {
-          breakpoint: 565,
-          yaxis: {
-            categories: [
-              "Jan",
-              "Feb",
-              "Mar",
-              "Apr",
-              "May",
-              "Jun",
-              "Jul",
-              "Aug",
-              "Sep",
-              "Oct",
-              "Nov",
-              "Dec",
-            ],
-          },
-          options: {
-            plotOptions: {
-              bar: {
-                horizontal: true,
-              },
-            },
-            chart: {
-              height: "550px",
-            },
-          },
-        },
-      ],
-    },
-  };
   return (
     <div className=" left-0 w-full py-5 px-2 lg:px-7 z-40">
       <div className="lg:ml-[260px] lg:w-[calc(100%-260px)]">
         <div className="w-full grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-7">
-          <div className="bg-[#f5f5f5] rounded-md">Hello</div>
+          <div className="bg-[#f5f5f5] rounded-md">
+            {/* Profile Image */}
+            <div className="flex justify-center items-center">
+              <img
+                className="h-[500px] w-[500px] rounded-full "
+                src="http://localhost:3000/images/admin.jpg"
+                alt=""
+              />
+            </div>
+
+            <div className="m-3 bg-gray-400 rounded-md p-2">
+              <h2>
+                <span className="font-bold">Name : </span>
+                Daksh Ahuja
+              </h2>
+              <h2>
+                <span className="font-bold">Email : </span>
+                adaksh679@gmail.com
+              </h2>
+              <h2>
+                <span className="font-bold">Role : </span>
+                Seller{" "}
+              </h2>
+              <h2>
+                <span className="font-bold">Status : </span>
+                Active
+              </h2>
+              <h2>
+                <span className="font-bold">Payment Account : </span>
+                Pending{" "}
+              </h2>
+            </div>
+
+            <div className="m-3">
+              <div>
+                <label htmlFor="shopName">Shop Name</label>
+                <input
+                  name="shopName"
+                  type="text"
+                  placeholder="Enter the shop name here..."
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                />
+              </div>
+              <div>
+                <label htmlFor="">Division Name</label>
+                <input
+                  name="divisionName"
+                  type="text"
+                  placeholder="Enter the division name here..."
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                />
+              </div>
+              <div>
+                <label htmlFor="">District Name</label>
+                <input
+                  name="districtName"
+                  type="text"
+                  placeholder="Enter the district name here..."
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                />
+              </div>
+              <div>
+                <label htmlFor="">Sub District </label>
+                <input
+                  name="subDistrict"
+                  type="text"
+                  placeholder="Enter the sub district name here..."
+                  className="w-full p-2 border border-gray-300 rounded-md"
+                />
+              </div>
+            </div>
+          </div>
 
           <div className="bg-[#f5f5f5] rounded-md p-4">
             <h2 className="font-bold text-xl py-2">Change Password</h2>
 
-            <label htmlFor="">Email</label>
-            <input
-              name="email"
-              type="text"
-              placeholder="Email"
-              className="w-full p-2 border border-gray-300 rounded-md"
-            />
+            <div className="py-1">
+              <label htmlFor="">Email</label>
+              <input
+                name="email"
+                type="text"
+                placeholder="Email"
+                className="w-full p-2 border border-gray-300 rounded-md"
+              />
+            </div>
 
-            <label htmlFor="">Old Password</label>
-            <input
-              name="oldPassword"
-              type="text"
-              placeholder="Old Password"
-              className="w-full p-2 border border-gray-300 rounded-md"
-            />
+            <div className="py-1">
+              <label htmlFor="">Old Password</label>
+              <input
+                name="oldPassword"
+                type="text"
+                placeholder="Old Password"
+                className="w-full p-2 border border-gray-300 rounded-md"
+              />
+            </div>
 
-            <label htmlFor="">New Password</label>
-            <input
-              name="newPassword"
-              type="text"
-              placeholder="New Password"
-              className="w-full p-2 border border-gray-300 rounded-md"
-            />
+            <div className="py-1">
+              <label htmlFor="">New Password</label>
+              <input
+                name="newPassword"
+                type="text"
+                placeholder="New Password"
+                className="w-full p-2 border border-gray-300 rounded-md"
+              />
+            </div>
+
+            <button className="w-1/4 bg-red-600 text-white p-2 rounded-md mt-2">
+              Save Changes
+            </button>
           </div>
         </div>
       </div>
