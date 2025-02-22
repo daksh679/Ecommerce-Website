e1e72138ddb8fd0ffa2f7e3225387a012a2c2738
Daksh Ahuja <adaksh679@gmail.com>
2025-02-22 22:50:21 +0530

    Seller Dashboard/Seller Register

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, Navigate } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
