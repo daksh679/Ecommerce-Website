e280e7795e9041a3c293f1a6451af9df8faa673c
Daksh Ahuja <adaksh679@gmail.com>
2025-02-27 22:46:15 +0530

    Seller Dashboard/Seller Register

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,6 +5,7 @@ const Header = ({ showSidebar, setShowSidebar }) => {
   return (
     <div className="fixed top-0 left-0 w-full py-5 px-2 lg:px-7 z-40">
       <div className="ml-0 lg:ml-[260px] rounded-md h-[65px] flex justify-between items-center bg-[#b1addf] px-5 transition-all ">
+        {/* Sidebar */}
         <div
           onClick={() => setShowSidebar(!showSidebar)}
           className="w-[35px] flex lg:hidden h-[35px] rounded-sm  bg-indigo-500 hover:shadow-indigo-500/50 items-center justify-center  cursor-pointer"
@@ -12,6 +13,7 @@ const Header = ({ showSidebar, setShowSidebar }) => {
           <FaList />
         </div>
 
+        {/* Search  */}
         <div className=" hidden md:block">
           <input
             className="px-3 py-2 outline-none bg-transparent border border-slate-700 rounded-md focus:border-indigo-300 overflow-hidden"
@@ -21,6 +23,7 @@ const Header = ({ showSidebar, setShowSidebar }) => {
           />
         </div>
 
+        {/* Profile */}
         <div className="flex justify-center items-center gap-8 relative">
           <div className="flex justify-center items-center">
             <div className="flex justify-center items-center gap-3">
