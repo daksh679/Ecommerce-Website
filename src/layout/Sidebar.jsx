589e29514f3e6e1ac38493beaeb8ca41957f0251
Daksh Ahuja <adaksh679@gmail.com>
2025-02-27 22:48:06 +0530

    Seller Dashboard/Seller Register

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -53,6 +53,7 @@ const Sidebar = ({ showSidebar, setShowSidebar }) => {
                 </Link>
               </li>
             ))}
+            {/* logout */}
             <li>
               <button className="text-[#030811] font-bold duration-200px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1 ">
                 <span>
